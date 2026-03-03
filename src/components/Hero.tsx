36706ce038887d95e7377fcfdead8b0fdfb062e7
Not-A-DevStudio <264600489+Not-A-DevStudio@users.noreply.github.com>
2026-03-03 16:11:57 +0800

    libjiezhu v.0.1.1 has released

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,7 +26,7 @@ export function Hero() {
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-2.5 w-2.5 bg-emerald-500"></span>
             </span>
-            <span className="font-medium mr-2">v0.1.0</span> <span className="opacity-80">available now</span>
+            <span className="font-medium mr-2">v0.1.1</span> <span className="opacity-80">available now</span>
           </motion.div>
 
           <motion.h1 
